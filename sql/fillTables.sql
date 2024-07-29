e103873f79c57a70f2c1592cbba01005c7cbfb3a
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-07-29 16:52:10 +0200

    fix: fix test table data

--- sql/fillTables.sql
+++ sql/fillTables.sql
@@ -14,10 +14,10 @@ insert into carts (user_id, created_at, updated_at, status) values
 
 -- Insert test data into cart_items table
 insert into cart_items (cart_id, product_id, count) values
-('6b6e5b1c-5b1f-4f85-a6f3-469261864b03', '3d7e5331-2bfc-4e2b-bb2b-c6446e6c316b', 1),
-('6fe2a0b6-0667-423f-8d39-250927c27771', '3366b555-617c-45d0-88ea-224f0225e489', 5),
-('5cfff37c-43e8-462e-bdfe-45bbe5d87286', 'c9901dce-24f6-458b-bdb6-7661db8d7f61', 2),
-('19ba1f39-cd20-48e7-b268-9eb28c4005bf', '73ad8198-af37-450f-9070-d1669af17a2e', 4);
+('373db379-22cc-4b8c-b226-f4e585ffbeb6', '3d7e5331-2bfc-4e2b-bb2b-c6446e6c316b', 1),
+('309522d0-3953-4359-b6be-537495e5dead', '3366b555-617c-45d0-88ea-224f0225e489', 5),
+('92707637-31fc-4c17-bafe-2ad57bd9e429', 'c9901dce-24f6-458b-bdb6-7661db8d7f61', 2),
+('403fb94a-f7e8-449f-9c97-e948b67eb016', '73ad8198-af37-450f-9070-d1669af17a2e', 4);
 
 -- Insert test data into products table
 insert into products (title, description, price) values
@@ -28,6 +28,9 @@ insert into products (title, description, price) values
 
 -- Insert test data into orders table
 insert into orders (user_id, cart_id, payment, delivery, comments, status, total) values
-('5669968e-b96c-46fd-9269-9c8f3ff154dd', '6b6e5b1c-5b1f-4f85-a6f3-469261864b03', '{}', '{}', 'I want to order this product', 'OPEN', 1),
-('7fa50bff-89f8-4005-9c0f-7cb0a374382d', '6fe2a0b6-0667-423f-8d39-250927c27771', '{}', '{}', 'I want to order this product', 'OPEN', 10);
+('5669968e-b96c-46fd-9269-9c8f3ff154dd', '373db379-22cc-4b8c-b226-f4e585ffbeb6', '{}', '{}', 'I want to order this product', 'OPEN', 1),
+('7fa50bff-89f8-4005-9c0f-7cb0a374382d', '309522d0-3953-4359-b6be-537495e5dead', '{}', '{}', 'I want to order this product', 'OPEN', 10);
+
+
+
 
